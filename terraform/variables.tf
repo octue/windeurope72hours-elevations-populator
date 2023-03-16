e29693bef850cc97540d989a6ad522e92b20e044
cortadocodes <cortado.codes@protonmail.com>
2023-03-16 16:50:06 +0000

    OPS: Add initial Terraform configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "organization" {
+  type = string
+  default = "octue"
+}
+
+variable "project" {
+  type    = string
+  default = "windeurope72-private"
+}
+
+variable "project_number" {
+  type = string
+  default = "406572174402"
+}
+
+variable "region" {
+  type = string
+  default = "europe-west1"
+}
+
+variable "github_organisation" {
+  type    = string
+  default = "octue"
+}
+
+variable "credentials_file" {
+  type    = string
+  default = "gcp-credentials.json"
+}
