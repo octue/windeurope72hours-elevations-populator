43ca585ab368c6f1161753dfbc9d72414f84d6d2
cortadocodes <cortado.codes@protonmail.com>
2023-03-21 16:59:01 +0000

    OPS: Factor out service namespace and name in Terraform config

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "crash_diagnostics" {
-  name                        = "octue-elevations-populator-private"
+  name                        = "${var.service_namespace}-${var.service_name}"
   location                    = "EU"
   force_destroy               = true
   uniform_bucket_level_access = true
