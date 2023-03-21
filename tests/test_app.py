de27b1432d08f6faaaf927bfcd28d201efac3ee8
cortadocodes <cortado.codes@protonmail.com>
2023-03-21 12:09:30 +0000

    TST: Test downloading and loading tile

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 import os.path
 import unittest
+from unittest.mock import patch
 
-from elevations_populator.app import App
+from elevations_populator.app import BUCKET_NAME, App
 
 
 REPOSITORY_ROOT = os.path.dirname(os.path.dirname(__file__))
@@ -23,6 +24,27 @@ class TestApp(unittest.TestCase):
             with self.subTest(latitude=latitude, longitude=longitude):
                 tile_reference_coordinate = app._get_tile_reference_coordinate(latitude, longitude)
                 self.assertEqual(tile_reference_coordinate, expected_result)
+
+    def test_download_and_load_elevation_tile(self):
+        """Test that elevation tiles can be downloaded and loaded correctly."""
+        app = App(None)
+        test_tile_s3_path = "Copernicus_DSM_COG_10_N54_00_W005_00_DEM/Copernicus_DSM_COG_10_N54_00_W005_00_DEM.tif"
+        test_tile_path = os.path.join(REPOSITORY_ROOT, "tests", "Copernicus_DSM_COG_10_N54_00_W005_00_DEM.tif")
+
+        with patch("elevations_populator.app.tempfile.NamedTemporaryFile") as mock_named_temporary_file:
+            with patch("elevations_populator.app.s3.download_fileobj") as mock_download_fileobj:
+                with patch("builtins.open"):
+                    mock_named_temporary_file.return_value.__enter__.return_value.name = test_tile_path
+                    tile = app._download_and_load_elevation_tile(latitude=54, longitude=-5)
+
+        # Check tile has been downloaded correctly.
+        self.assertEqual(mock_download_fileobj.call_args[0][0], BUCKET_NAME)
+        self.assertEqual(mock_download_fileobj.call_args[0][1], test_tile_s3_path)
+        self.assertEqual(app._downloaded_files, [test_tile_path])
+
+        # Check tile has been loaded successfully.
+        self.assertEqual(tile.count, 1)
+        self.assertEqual(tile.name, test_tile_path)
 
     def test_get_tile_path(self):
         """Test that the path of the tile containing the given latitude and longitude is constructed correctly."""
