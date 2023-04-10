5a14beff1256a659c5a200d2221a918042eead41
cortadocodes <cortado.codes@protonmail.com>
2023-04-10 16:23:31 +0100

    REF: Simplify mocking in `get_elevations` script

--- scripts/get_elevations.py
+++ scripts/get_elevations.py
@@ -2,6 +2,7 @@ import os
 from unittest.mock import patch
 
 import h3
+import rasterio
 from octue import Runner
 
 from elevations_populator.app import App
@@ -10,6 +11,8 @@ from elevations_populator.app import App
 REPOSITORY_ROOT = os.path.dirname(os.path.dirname(__file__))
 TEST_TILE_PATH = os.path.join(REPOSITORY_ROOT, "tests", "Copernicus_DSM_COG_10_N54_00_W005_00_DEM.tif")
 
+# Avoid deleting test tile at end of app run.
+App.DELETE_DOWNLOADED_FILES_AFTER_RUN = False
 
 runner = Runner(app_src=App, twine="../twine.json")
 
@@ -18,12 +21,10 @@ initial_h3_cell = "8d19507316da43f"
 neighbours = h3.k_ring(initial_h3_cell, k=90)
 
 
-with patch("elevations_populator.app.tempfile.NamedTemporaryFile") as mock_named_temporary_file:
-    with patch("elevations_populator.app.s3.download_fileobj") as mock_download_fileobj:
-        with patch("builtins.open"):
-            with patch("os.remove"):
-                mock_named_temporary_file.return_value.__enter__.return_value.name = TEST_TILE_PATH
-
-                analysis = runner.run(input_values={"h3_cells": [*neighbours, initial_h3_cell]})
+with patch(
+    "elevations_populator.app.App._download_and_load_elevation_tile",
+    return_value=rasterio.open(TEST_TILE_PATH),
+):
+    analysis = runner.run(input_values={"h3_cells": [*neighbours, initial_h3_cell]})
 
 a = 3
