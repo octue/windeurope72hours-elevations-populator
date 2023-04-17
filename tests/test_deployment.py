ffe80fc949ff7f44859bdfd70dab662b79398f9e
cortadocodes <cortado.codes@protonmail.com>
2023-04-17 18:43:53 +0100

    TST: Add deployment test
    skipci

---
+++ tests/test_deployment.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+from unittest import TestCase
+
+from octue.resources import Child
+
+
+@unittest.skipUnless(
+    condition=os.getenv("RUN_CLOUD_RUN_DEPLOYMENT_TEST", "0").lower() == "1",
+    reason="'RUN_CLOUD_RUN_DEPLOYMENT_TEST' environment variable is False or not present.",
+)
+class TestCloudRunDeployment(TestCase):
+    # This is the service ID of the example service deployed to Google Cloud Run.
+    child = Child(
+        id="octue/elevations-populator-private:0-2-1",
+        backend={"name": "GCPPubSubBackend", "project_name": "windeurope72-private"},
+    )
+
+    def test_cloud_run_deployment(self):
+        """Test that the Google Cloud Run example deployment works, providing a service that can be asked questions and
+        send responses.
+        """
+        answer = self.child.ask(input_values={"h3_cells": [594920487381893119]})
+
+        # Check the output values.
+        self.assertIsNone(answer["output_values"])
