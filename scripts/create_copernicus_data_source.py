4ef4d312d4ce2e665b48c1b04f95c4255adcce56
cortadocodes <cortado.codes@protonmail.com>
2023-04-18 17:03:07 +0100

    ENH: Add script for creating Copernicus data source in database

---
+++ scripts/create_copernicus_data_source.py
@@ -0,0 +1,8 @@
+from elevations_populator.storage import (
+    COPERNICUS_GLO_30_DATA_SOURCE_NAME,
+    COPERNICUS_GLO_30_DATA_SOURCE_URI,
+    create_data_source,
+)
+
+
+create_data_source(COPERNICUS_GLO_30_DATA_SOURCE_NAME, COPERNICUS_GLO_30_DATA_SOURCE_URI)
