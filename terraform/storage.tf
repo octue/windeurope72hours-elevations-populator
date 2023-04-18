6b8555700dde1d48e54c1ecdcab039e7cbba4d63
cortadocodes <cortado.codes@protonmail.com>
2023-04-18 15:56:28 +0100

    REF: Move terraform config to API repo

--- terraform/storage.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "crash_diagnostics" {
-  name                        = "${var.service_namespace}-${var.service_name}"
-  location                    = "EU"
-  force_destroy               = true
-  uniform_bucket_level_access = true
-}
